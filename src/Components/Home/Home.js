694f20566168bef9c3ebfa5049f2f7415ebbdae2
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-03-10 21:12:57 +0600

    first page display done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,27 +3,29 @@ import Leagues from '../Leagues/Leagues';
 import './Home.css'
 
 const Home = () => {
-    const[leagues, setLeagues]=useState([])
-    useEffect(()=>{
-            const url = `https://www.thesportsdb.com/api/v1/json/1/all_leagues.php`
-            fetch(url)
-            .then(res=> res.json())
-            .then(data=>setLeagues(data.leagues))
-    },[])
-    // console.log(leagues)
-    const shortLeagues = leagues.splice(0,15);
+  const [leagues, setLeagues] = useState([])
+  useEffect(() => {
+    const url = `https://www.thesportsdb.com/api/v1/json/1/all_leagues.php`
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setLeagues(data.leagues))
+  }, [])
+  // console.log(leagues)
+  const shortLeagues = leagues.splice(0, 15);
+
+  
 
 
-     return (
-        <div>
-        {
-      shortLeagues.map(league=>{
-        return<Leagues league ={league}/>
-      })
-    }
+  return (
+    <div className="row m-2">
+      {
+        shortLeagues.map(league => {
+          return <Leagues league={league} />
+        })
+      }
 
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/Leagues/Leagues.js
+++ src/Components/Leagues/Leagues.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Leagues.css'
 
 const Leagues = (props) => {
     const{idLeague,strLeague,strSport} = props.league;
     console.log(strLeague);
    
     return (
-        <div>
-          <h2>{strLeague}</h2>
-            <h4>Sports Type: {strSport}</h4>
-            <Link to ={`/details/${idLeague}`}><button>Explorer</button></Link>
-            
+        <div className="col-lg-4 col-md-6 p-4  custom-box">
+            <div className="card">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div className="card-body">
+             <h4 className="card-title">{strLeague}</h4>
+             <h5 className="card-text" >Sports Type: {strSport}</h5>
+            <Link  className ="btn btn-primary" to ={`/details/${idLeague}`}>Explorer</Link>
+
+            </div>
+            </div>
         </div>
     );
 };
